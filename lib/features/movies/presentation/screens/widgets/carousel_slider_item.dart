bb08ddd7f11ff0febafd56f49cae79027cb28b02
Duong Lam <duongsulam1234@gmail.com>
2025-03-14 15:46:31 +0700

    Update Black Container With Low Opacity

--- lib/features/movies/presentation/screens/widgets/carousel_slider_item.dart
+++ lib/features/movies/presentation/screens/widgets/carousel_slider_item.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:smoth_movie_app/common/model/movie_detail_param_model.dart';
+import 'package:smoth_movie_app/common/widgets/cached_network/container_with_cached_network_image_provider.dart';
 import 'package:smoth_movie_app/common/widgets/responsive_small_text.dart';
 import 'package:smoth_movie_app/features/movies/domain/entities/currently_update_movies/recently_update_list_item.dart';
-import 'package:smoth_movie_app/common/widgets/cached_network/cached_network_image.dart';
+import 'package:smoth_movie_app/router/app_router.dart';
 
 class CarouselSliderItem extends StatelessWidget {
   const CarouselSliderItem({super.key, required this.item});
@@ -11,27 +12,34 @@ class CarouselSliderItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //
     // final sHeight = MediaQuery.of(context).size.height;
+    final sWidth = MediaQuery.of(context).size.width;
     return GestureDetector(
       onTap: () => Navigator.pushNamed(
         context,
-        "movie_detail",
+        AppRouter.movieDetail,
         arguments: MovieDetailParamModel(slug: item.slug),
       ),
       child: Stack(
         children: [
-          CachedNetworkImageWidget(
-            url: item.posterUrl,
-            width: double.infinity,
+          ContainerWithCachedNetworkImageProvider(
+            path: item.posterUrl,
+            width: sWidth,
           ),
           Positioned(
             bottom: 0,
-            left: 10,
-            child: ResponsiveText(
-              text: item.name,
-              fontSize: 20,
-              textColor: Colors.white,
+            child: Container(
+              height: 40,
+              width: sWidth,
+              padding: const EdgeInsets.only(left: 5, top: 5),
+              decoration: BoxDecoration(
+                color: Colors.black.withValues(alpha: 0.15),
+              ),
+              child: ResponsiveText(
+                text: item.name,
+                fontSize: 20,
+                textColor: Colors.white,
+              ),
             ),
           ),
         ],
