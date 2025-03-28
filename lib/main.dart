b17fc52df4591df4ab032ec6844227198041f289
Duong Lam <duongsulam1234@gmail.com>
2025-03-28 18:04:11 +0700

    Fix: flutter app name doesn't show when see opened apps

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Movies For Free",
       debugShowCheckedModeBanner: false,
       theme: AppTheme.blackTheme,
       onGenerateRoute: (settings) => AppRouter().onGenerateRoute(settings),
