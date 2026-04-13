20f71fc10483ad1dddd3cc4d360b2d089547ec8d
duongday <duong.lam@aegona.com>
2026-04-13 14:21:07 +0700

    feat: implement MyApp class and update bootstrap to use it

--- lib/core/bootstrap.dart
+++ lib/core/bootstrap.dart
@@ -1,9 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:smoth_movie_app/core/router/app_router.dart';
+import 'package:smoth_movie_app/core/app.dart';
 import 'package:smoth_movie_app/core/utils/helper/helper.dart';
-import 'package:smoth_movie_app/core/utils/theme/theme.dart';
 import 'package:smoth_movie_app/core/init_dependencies.dart';
 
 Future<void> bootstrap() async {
@@ -15,19 +14,4 @@ Future<void> bootstrap() async {
 
   // Run the app
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Movies For Free",
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.blackTheme,
-      initialRoute: AppRouter.splash,
-      onGenerateRoute: (settings) => AppRouter().onGenerateRoute(settings),
-    );
-  }
 }
