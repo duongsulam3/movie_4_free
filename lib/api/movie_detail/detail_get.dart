9a8079bb7ac4857b7e160d5c989186f5ef76c17e
duongday <duong.lam@aegona.com>
2026-04-15 16:26:20 +0700

    feat: add DetailMovieGETAPI for fetching movie details and refactor DetailMovieRemoteDataSourceImpl to utilize the new API

---
+++ lib/api/movie_detail/detail_get.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:smoth_movie_app/core/utils/network/app_service.dart';
+import 'package:smoth_movie_app/core/utils/network/types.dart';
+import 'package:smoth_movie_app/core/utils/secret/api_end_point.dart';
+
+final class DetailMovieGETAPI {
+  static const String endpoint = ApiEndPoint.detailMovieEndpoint;
+
+  static Future<Response<Json>> apiDetailGETMovie({
+    required AppService client,
+    required String slug,
+  }) async {
+    return client.getRequest('$endpoint$slug');
+  }
+}
--- lib/features/movie_detail/data/source/remote/detail_movie_remote_data_source.dart
+++ lib/features/movie_detail/data/source/remote/detail_movie_remote_data_source.dart
@@ -1,6 +1,6 @@
+import 'package:smoth_movie_app/api/movie_detail/detail_get.dart';
 import 'package:smoth_movie_app/core/error/exception.dart';
 import 'package:smoth_movie_app/core/utils/helper/helper.dart';
-import 'package:smoth_movie_app/core/utils/secret/api_end_point.dart';
 import 'package:smoth_movie_app/core/utils/network/app_service.dart';
 import 'package:smoth_movie_app/features/movie_detail/data/model/movie_detail.dart';
 
@@ -11,11 +11,14 @@ abstract interface class DetailMovieRemoteDataSource {
 class DetailMovieRemoteDataSourceImpl implements DetailMovieRemoteDataSource {
   final AppService client;
   const DetailMovieRemoteDataSourceImpl({required this.client});
+
   @override
   Future<MovieDetailModel> getDetailMovie({required String slug}) async {
     try {
-      final endpoint = "${ApiEndPoint.detailMovieEndpoint}$slug";
-      final res = await client.getRequest(endpoint);
+      final res = await DetailMovieGETAPI.apiDetailGETMovie(
+        client: client,
+        slug: slug,
+      );
       return Helper.parseMovieDetail(res.toString());
     } catch (e) {
       throw ServerException(e.toString());
