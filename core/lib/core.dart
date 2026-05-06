ffbffe507ea36dcaadb2282854790d3219d32b63
duongday <duong.lam@aegona.com>
2026-05-06 17:16:03 +0700

    refactor/add spacing for improved readability in core.dart

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -1,7 +1,15 @@
+library;
+
+/** ROUTES **/
 export 'navigation/super_app_error_page.dart';
 export 'navigation/super_app_route.dart';
+
+/** NETWORK **/
 export 'network/abstract_dio_client.dart';
 export 'network/graphql_api_client.dart';
 export 'network/restful_api_client.dart';
 export 'network/token_management_mixin.dart';
+
+/** UTILS **/
+/*** RESPONSIVE ***/
 export 'responsive/responsive.dart';
