936547497938cfb2b4182cb4997ac495bebb7742
duongday <duong.lam@aegona.com>
2026-05-26 16:44:52 +0700

    optimize/compare_using_equal_package_instead_of_compare_as_json

--- lib/features/movies/data/source/local/home_movies_cache_compare.dart
+++ lib/features/movies/data/source/local/home_movies_cache_compare.dart
@@ -1,5 +1,4 @@
-import 'dart:convert';
-
+import 'package:flutter/foundation.dart';
 import 'package:smoth_movie_app/features/movies/data/model/recently_update_movies/recently_update_list_item_model.dart';
 import 'package:smoth_movie_app/features/movies/data/model/single_movies/movie_item_model.dart';
 
@@ -10,19 +9,13 @@ class HomeMoviesCacheCompare {
     List<MovieItemModel> oldData,
     List<MovieItemModel> newData,
   ) {
-    if (oldData.length != newData.length) return false;
-    final oldJson = jsonEncode(oldData.map((e) => e.toJson()).toList());
-    final newJson = jsonEncode(newData.map((e) => e.toJson()).toList());
-    return oldJson == newJson;
+    return listEquals(oldData, newData);
   }
 
   static bool recentlyEquals(
     List<RecentlyUpdateListItemModel> oldData,
     List<RecentlyUpdateListItemModel> newData,
   ) {
-    if (oldData.length != newData.length) return false;
-    final oldJson = jsonEncode(oldData.map((e) => e.toJson()).toList());
-    final newJson = jsonEncode(newData.map((e) => e.toJson()).toList());
-    return oldJson == newJson;
+    return listEquals(oldData, newData);
   }
 }
